e07884d28ab0346c624778915e4a967abe450c8d
axteure <clement.hanquiez@gmail.com>
2023-12-03 17:36:55 +0100

    The MyMusicVerse main smart contract

---
+++ backend/contracts/MyMusicVerse.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity 0.8.20;
+
+import "./Crowdfunding.sol";
+import "./MyUSD.sol";
+
+contract MyMusicVerse {
+
+    MyUSD myUSD;
+
+    mapping(address => address[]) public artistCampaigns;
+
+    event CampaignCreated(address campaignAddress, string title);
+
+    constructor(address _myUSDAddress) {
+        myUSD = MyUSD(_myUSDAddress);
+    }
+
+    function createCampaign (uint32 _target, string calldata _title) external {
+        require(_target >= 1000, "The minimum target to create a campaign is 1000 MUSD.");
+        require(bytes(_title).length > 0, "The title of the campaign cannot be empty.");
+
+        uint256 timestamp = block.timestamp;
+
+        address campaignAddress = address(
+            new Crowdfunding{salt: keccak256((abi.encodePacked(timestamp, msg.sender, _target, _title)))}(address(myUSD),msg.sender, _target, _title)
+        );
+
+        artistCampaigns[msg.sender].push(campaignAddress);
+
+        emit CampaignCreated(campaignAddress, _title);
+    }
+
+    function getArtistCampaigns(address artistAddress) external view returns (address[] memory) {
+        return artistCampaigns[artistAddress];
+    }
+
+	
+}
