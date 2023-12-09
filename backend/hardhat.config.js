4a83d054b1c0532c062e020aee25d188a3165423
axteure <clement.hanquiez@gmail.com>
2023-12-09 21:56:26 +0100

    Add feed script & Sepolia configuration for deployment

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,7 +1,31 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv/config");
+require("@nomicfoundation/hardhat-verify");
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.20",
-  settings: { optimizer: { enabled: true, runs: 200 } }
+  solidity: { compilers: [ { version: "0.8.20" } ] },
+  settings: { optimizer: { enabled: true, runs: 200 } },
+  networks: {
+    sepolia : {
+      url: SEPOLIA_RPC_URL,
+      accounts: [`0x${PRIVATE_KEY}`],
+      chainId: 11155111,
+      blockConfirmations: 6,
+    },
+    localhost : {
+      url: "http://127.0.0.1/8545",
+      chainId: 31337
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  sourcify: {
+    enabled: false,
+  }
 }
